9eccf6ad8136776cf887a9d140145a487e77556c
Huy DT <dthuytech@gmail.com>
2023-04-15 11:28:52 +0700

    dcdf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,37 @@
 ﻿using login.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using login.Models;
+using login.Data;
 
 namespace login.Controllers
 {
    
     public class HomeController : Controller
     {
+
+      
+        IList<Food> listFinal = new List<Food>();
+        List<FoodContent> foodContents = new List<FoodContent>();
+
+        rbDBContext _typeFoodDBContext;
+
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<rbDBContext> _userManager;
 
 
-       
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, rbDBContext applicationDbContext/*UserManager<ApplicationDbContext> userManager*/)
         {
+            //_userManager = userManager;
             _logger = logger;
+            _typeFoodDBContext = applicationDbContext;
+            listFinal = _typeFoodDBContext.Foods.ToList();
+
+            _logger = logger;
+            foodContents = _typeFoodDBContext.foodContents.ToList();
         }
 
         public IActionResult Index()
