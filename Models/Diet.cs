404fc66e4a29a82b3b4923bbee7a2b73d4a19aa0
minhHieu1803 <phamhieu5434@gmail.com>
2023-04-21 09:23:39 +0700

    ok

---
+++ Models/Diet.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+using System.Net.NetworkInformation;
+using System.ComponentModel.DataAnnotations;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace login.Models
+{
+    public class Diet
+    {
+        [Key]
+        public int IdDiet { get; set; }
+        public int Buoi { get; set; }
+        public int Day { get; set; }
+
+        [ForeignKey("User")]
+
+        public int IdUser { get; set; }
+
+        [ForeignKey("Foods")]
+        public int IdFood { get; set; }
+    }
+}
