c9462fce484e90eb49c54a67cd1ba3da69e6b979
minhHieu1803 <phamhieu5434@gmail.com>
2023-05-14 16:27:16 +0700

    đang fix

--- wwwroot/js/swiper.js
+++ wwwroot/js/swiper.js
@@ -3,7 +3,12 @@
     grabCursor: true,
     centeredSlides: true,
     loop: true,
-    slidesPerView: 'auto',
+    spaceBetween: 30,
+    slidesPerView: '2',
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
     coverflowEffect: {
         rotate: 0,
         stretch: 0,
@@ -18,4 +23,38 @@
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
+});
+
+var team_slider = new Swiper(".team-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1.2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    },
 });
