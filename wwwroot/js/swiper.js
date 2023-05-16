3b6131797894c97db1b5bee72d2c2b8814dce8bf
minhHieu1803 <phamhieu5434@gmail.com>
2023-05-16 09:24:32 +0700

    fix swiper slide , recipes

--- wwwroot/js/swiper.js
+++ wwwroot/js/swiper.js
@@ -1,29 +1,59 @@
-﻿var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
+﻿//var TrandingSlider = new Swiper('.tranding-slider', {
+//    effect: 'coverflow',
+//    grabCursor: true,
+//    centeredSlides: true,
+//    loop: true,
+//    spaceBetween: 30,
+//    slidesPerView: '2',
+//    autoplay: {
+//        delay: 1500,
+//        disableOnInteraction: false,
+//    },
+//    coverflowEffect: {
+//        rotate: 0,
+//        stretch: 0,
+//        depth: 100,
+//        modifier: 2.5,
+//    },
+//    pagination: {
+//        el: '.swiper-pagination',
+//        clickable: true,
+//    },
+//    navigation: {
+//        nextEl: '.swiper-button-next',
+//        prevEl: '.swiper-button-prev',
+//    }
+//});
+var TrandingSlider = new Swiper(".tranding-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
     loop: true,
-    spaceBetween: 30,
-    slidesPerView: '2',
     autoplay: {
-        delay: 1500,
+        delay: 3000,
         disableOnInteraction: false,
     },
+    speed: 2000,
+    effect: "coverflow",
     coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
+        rotate: 3,
+        stretch: 2,
         depth: 100,
-        modifier: 2.5,
+        modifier: 5,
+        slideShadows: false,
+    },
+    loopAdditionSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
         clickable: true,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
 });
+
+
+
 
 var team_slider = new Swiper(".team-slider", {
     slidesPerView: 3,
